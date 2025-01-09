3b91d713d6f650bea97086c4c968df270ba2fc18
youchenlong <2091838306@qq.com>
2025-01-09 12:10:11 +0800

    status

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -2,6 +2,8 @@ REGISTRY = {}
 
 from .basic_controller import BasicMAC
 from .dsr_controller import DSRMAC
+from .maddpg_controller import MADDPGMAC
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["dsr_mac"] = DSRMAC
+REGISTRY["maddpg_mac"] = MADDPGMAC
--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -2,6 +2,7 @@ from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
 from .latent_q_learner import LatentQLearner
+from .maddpg_learner import MADDPGLearner
 
 REGISTRY = {}
 
@@ -9,3 +10,4 @@ REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["qtran_learner"] = QTranLearner
 REGISTRY["latent_q_learner"] = LatentQLearner
+REGISTRY["maddpg_learner"] = MADDPGLearner
